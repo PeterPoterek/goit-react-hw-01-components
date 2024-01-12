c56868555e7eaad3e8d6683e54b2d3364b98af41
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-12 18:11:49 +0100

    npm install styled-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-      }}
-    >
-      test
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
   );
 };
