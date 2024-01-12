9c2b53dbe3b403c9e0d909684aef05c2c6e8a7b4
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-12 21:28:25 +0100

    FriendList error fixed

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -13,9 +13,13 @@ const FriendList = ({ friends }) => {
     <FriendListContainer>
       <FriendListUl>
         {friends.map(friend => {
+          const statusColor = {
+            backgroundColor: friend.isOnline ? 'green' : 'red',
+          };
+
           return (
             <FriendListItem key={friend.id}>
-              <Status isOnline={friend.isOnline} />
+              <Status style={statusColor} />
               <Avatar src={friend.avatar} />
               <Name>{friend.name}</Name>
             </FriendListItem>
--- src/components/FriendList/FriendListStyles.js
+++ src/components/FriendList/FriendListStyles.js
@@ -29,7 +29,7 @@ export const Status = styled.span`
   width: 10px;
   height: 10px;
   border-radius: 50%;
-  background-color: ${props => (props.isOnline ? 'green' : 'red')};
+  background-color: red;
   margin-right: 10px;
 `;
 
